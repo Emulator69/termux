b3abb9b4b48d70db05f06c4904d8e769ae62b4bb
gellmoxer <50492405+gellmoxer@users.noreply.github.com>
2019-05-10 22:45:57 +0700

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+jl='SED'
+jd='JAD'
+O='-'
+rtws=' :'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+txcwq='te'
+kpg=']'
+amrk='USA'
+lkrq=' }'
+sr='up-st'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+fchj='💲'
+fchk='AND'
+gftd='ANG'
+zx='3'
+awcrx=':'
+xrsd=')'
+jbkn='trap'
+njroh='in'
+sp='set'
+rewyd='SAV'
+bxrs='r'
+ftry='{'
+dmck='FREEZE'
+endc='\E[0m'
+slnmb='...'
+nx='K'
+nb='AK '
+jg='AN'
+mkljhgb='echo'
+azrcx='pil'
+df='cat'
+caft='A'
+cgfrt='A T'
+rpg='['
+rpkd='('
+smpkijo='cd'
+tryn='ur'
+twrq=':'
+trym='or'
+nmrt='ID'
+ghg='EG²'
+gsr='I G'
+jwbnunbkl='r'
+bsek='clear'
+grr='|'
+blue='\e[1;34m'
+zc='2'
+true=':'
+cxvz='ung '
+wrt='lol'
+trc='sleep'
+kcxt='INT'
+drtsw='mux'
+ptky='enjoy'
+ijhkxc='ls'
+itafg='figlet'
+ytwq=' JAT'
+ddh='H D'
+ertfd='Hit'
+umr='age'
+umt='AGE'
+erty='»'
+ltrzs=';'
+bcxmn='ctr'
+cyan='\e[1;36m'
+smb='         '
+kcvt='etik '
+zv='1'
+jwm='UH C'
+zljq=' &'
+gfhjd='read'
+jmbutbnn='lagi'
+xcghjtr='exit'
+obh='l_c'
+vbcfg='Mund'
+red='\e[1;31m'
+smcv='   '
+yll='\e[1;33m'
+xx='__________'
+$mkljhgb ""
+###################################################
+#    SCRIPT ini Sengaja Saya tulis seperti apa    #
+#        yang ada di dalam Hati CEWEK             #
+#  Makanya Sulit diPahami ataupun di Mengerti :v  #
+#     Jadi Silahkan Recode Sesuka anda :v         #
+#  ,-Mario Bogell :v                              #
+#                                                 #
+###################################################
+$jbkn $bcxmn$obh $kcxt
+ctrl_c() {
+$bsek
+$mkljhgb ""
+$mkljhgb -e "Metu cuk"|$wrt$df
+$trc $zc
+$bsek
+$xcghjtr
+}
+cxzv=$zv
+while [ $cxzv -lt $zc ];
+do
+$mkljhgb -e $blue "$nx$kcvt$g$erty$smcv$jwbnunbkl";
+$mkljhgb ""
+$gfhjd -p "$smb$fchj"$smcv$azrcx;
+case $pil in
+$jwbnunbkl)
+$mkljhgb ""
+$txcwq$bxrs$drtsw$O$sp$sr$trym$umr
+$mkljhgb -e "$smb$ertfd$cxvz$vbcfg$tryn$slnmb"|$wrt$df
+$trc $zc
+$mkljhgb "$smcv$smb$smcv$smb$zx"|$wrt$df
+$trc $zv
+$mkljhgb "$smcv$smb$smcv$smb$zc"|$wrt$df
+$trc $zv
+$mkljhgb "$smcv$smb$smcv$smb$zv"|$wrt$df
+$trc $zv
+$mkljhgb -e "$smb$fchk$cgfrt$nmrt$nb$jl$gftd$ytwq$jwm$kcxt$caft"|$wrt$df
+$trc $zv
+$mkljhgb -e "$smb$jd$gsr$caft$amrk$ddh$ghg$nx$jg$slnmb"|$wrt$df
+$trc $zc
+$itafg "$dmck$slnmb"|$wrt$df
+$ucui$hudgjhc$hlkjgcc$chxtcjs$axfgvmlk$fyioy
+$xffvjj$axrrd$zxcvgdf$xznff$sdxhcg$etsaf$yyxctj$azrgh
+$juyyfi$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fyth
+$lhifyg$xffvjj$axrrd$zxcvgdf$xznff$qrrgjh$amrklhg
+$lkjgcc$chxtcjs$axfgvmlk$fyioy$sdxhcg$olloj$zvsfv$dytu
+$adhkhy$agcxt$sdxhcg$olloj$zvsfv$dytuki$phuvxsl
+$uckfi$ycyy$ykk$adhkhy$agcxt$sdxhcg$olloj$zvsfgv$rsgg
+$ycyy$ykk$adhkhy$agcxt$sdxhcg$etsaf$yyxctj$azrgh
+$sdxhcg$olloj$zvsfv$dytu$ycyy$ykk$adhkhy$agcxt
+$yrsttzxc$hlkjgcc$chxtcjs$axfgvmlk$fyioy$tyreyx
+$ohfvjj$axrrd$zxcvgdf$xznff$sdxhcg$etsaf$yyxctj$aihrgh
+$sdyyfi$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fygyh
+$adfyg$xffvjj$axrrd$zxcvgdf$xznff$qrrgjh$amrklyt
+$qwrjgcc$chxtcjs$axfgvmlk$fyioy$sdxhcg$olloj$cctytr
+$iytkhy$agcxt$sdxhcg$olloj$zvsfv$dytuki$phuvxtd
+$pwqfi$ycyy$ykk$adhkhy$agcxt$sdxhcg$olloj$cctyrt
+$kgfy$ykk$adhkhy$agcxt$sdxhcg$etsaf$yyxctj$azrdh
+$mbchcg$olloj$zvsfv$dytu$ycyy$yhkk$adhkhy$agctre
+$hlkjgcc$chxtcjs$axfgvmlk$fyioy$xffvjj$axrrd
+$xxcvgdf$xznff$sdxhcg$etsaf$yyxctj$azrgh$juyyfi
+$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fyth$lhifyg
+$xffvjj$axrrd$zxcvgdf$xznff$qrrgjh$amrklhg$lkjgcc
+$chxtcjs$axfgvmlk$fyioy$sdxhcg$omlloj$zvsfv$dygtu$adhkhy
+$agcxt$sdxhcg$ollbj$zvsfv$dytuki$phuvxsl$uckfi
+$werxyy$ykfdk$adhkhy$agcxt$sdxhcg$olloj$zvsfv$dhytu
+$adhkhy$agcxt$sdxhcg$etsaf$yyxctj$azrgh$sdxhcg$guiloj
+$tydhjf$oixuft$zvsfhv$dydtu$ycyy$ykk$adhkhy$agcxt$yrsttzxc
+$hlkjgcc$chxtcjs$axfgvmlk$fyioy$ohfvjj$axrrd$zxcvgdf
+$xznff$sdxhcg$etsaf$yyxctj$aihrgh$sdyyfi$yioytf
+$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fygyh$adfyg
+$xffvjj$axrrd$zxcvgdf$xznff$qrrgjh$furxig$amrklyt
+$qwrjgcc$chxtcjs$axfgvmlk$fyioy$sdxhcg$olloj$zvsfvu
+$iytkhy$agcxt$sdxhcg$olloj$zvshfv$dytuki$phuvxtd$pwqfi
+$ycyy$ykk$adhkhy$agcxt$sdxhcg$uifkcbloj$zvofvu
+$sdxhcyg$etsaf$yyxctj$azrdh$mbchcg$ollfdoj
+$zvsfv$dytu$ycyy$ykk$adhkhy$agctre$ytdjut$agxgj
+$axrrd$zxcvgdf$xznff$etsaf$yyxctj$aihrgh
+$sdyyfi$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fygyh
+$adfyg$xffvjj$axrrd$zxcvgdf$xznff$qrrgjh$amrklyt
+$qwrjgcc$chxtcjs$axfgvmlk$fyioy$sdxhchhg$xzvsfvu
+$iytkhy$agcxt$sdxhcg$vcolloj$zvsfvk$dytuki$phuvxtd
+$pwqfi$ycyy$hauykk$adhkhy$agcvtxt$zvsfvu$thpuagf
+$gkgfy$ykk$adhkhy$ajgcxt$sdxhcgd$ejltsa$yyxctj$azrdh
+$mbchcfg$hoolloj$zvsfv$dytu$ycyy$ykkiwu$adhkhy$agctre
+$hlkjgcc$chxtcjs$axfgvmlk$fyioy$xffvjj$axrrd
+$xxcvgdf$xznff$sdxhcg$etsaf$yyxgctj$azrgh$juyyfi
+$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fyth$lhifyg
+$xffvjj$axrrdg$zxcgvgdf$xrznff$qrrgjh$amrklhg$lkjgcc
+$chxtgcjs$axfgvmlk$fyioy$sdxhcg$olloj$sov$dytuoy$adhkhy
+$agcxtf$sdxhcg$orlloj$zvgsfv$dytuki$phuvl$uckfi
+$werxyy$cznykk$adhkhy$agcxht$sdtxhcg$ouwlloj$zvhsfv$dytu
+$adhkhy$agcxtb$sdxhcg$etsaf$yyxctj$azrgh$sxhcg$maklloj
+$tydhjf$oixuft$zvgsfuv$dytu$yzcyy$yklkk$ohkhy$aigcxt
+$zxcvgdf$xznff$sdxhcg$etsaf$yyxctj$aihrgh
+$sdyyfi$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$ltokfz$fyugyh
+$adfyg$xffvjj$axrrd$zxcvgdf$xzunff$qrrgjh$amrklyt
+$qwrjgcc$chxtcjs$axfgvmlk$fyioy$sdxhcgh$yklloyj$zovsfvu
+$iytkhy$agcxt$sdxuhcg$haluoj$zvspfv$dytuki$phuvx
+$pwqfi$ycyy$ykk$adhkhy$agcxt$sdxhcg$aploj$sgsfvuh
+$kgfy$ykqfk$adhkhy$aqgcxt$sdpxhcg$etsaf$yiyxctj$azrdh
+$mbchcg$olloj$zvssfv$dytu$ycyy$ykysk$adhkhy$agctre
+$hlkjgcc$chxhtcjs$axfgvmlk$fyioy$xffvjj$kaxrrd
+$xxcvgdf$xznff$sdxhgcg$etsaf$yyxctj$azrgh$juyyfi
+$yioytf$tdtyx$aqsrr$tyjfgo$aexdtx$atokfz$fyth$lhifyg
+$xffvjj$axrrd$zxcvgdf$xznff$qrrgjh$amrklhg$lkjgcc
+$chxtcjs$axfgvmlk$fyioy$sdxhcg$olloj$zvsfvhu$gdytu$adhkhy
+$agcxt$sdxhcgwg$oblloj$zvsfvv$dytuki$phuvxsl$uckfi
+$werxyy$ykk$adhuwkhy$agcxt$sdhxhcg$klolloj$zvsfv$dytu
+$adhkhy$agcxt$sdxhcg$etsaf$yyxctj$azrgh$sdxhcg$olloj
+$tydhjf$oixuft$zvgsfv$dytu$ycyy$ydukk$adhkhy$apogcxt
+$xcghjtr$smb$smcv$smcv$smb
+;;
+*) echo -e $cyan  "        -= SEPERTINYA ANDA SALAH INPUT =-"
+echo -e $g "               -= MASUK LAGI =-"
+echo -e "$xx$xx$xx$xx$xx"|$wrt$df
+esac
+done
+done
