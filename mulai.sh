ec37f4443919c77101f9836310c316956989dee8
GellMoxer <50492405+gellmoxer@users.noreply.github.com>
2019-06-09 01:54:41 +0700

    Update mulai.sh

--- mulai.sh
+++ mulai.sh
@@ -1,7 +1,13 @@
 #Start TOOLSGM
 
+#ngasih izin toolsnya
 chmod +x ToolsGM.sh
+#pindahin ke home
 mv ToolsGM.sh ../
+#ke home yang sbelumnya di folder termux
 cd $home
+#menghapus folder termux yang udah gak digunakan
+rm -rf termux
 clear
+#mulai Jalankan Toolsnya 
 bash ToolsGM.sh
